066618acd189669b31235fe15e342ae69b23b544
gurusha47 <gurusha47@gmail.com>
2024-09-05 21:46:52 +0100

    Add files via upload

---
+++ YOLOv5_BiFPN_TRANSFORMER/train_5x_bt2.py
@@ -0,0 +1,76 @@
+import os
+import subprocess
+import torch 
+import yaml
+import gc
+
+# Clear memory of garbage and cache files
+gc.collect()
+torch.cuda.empty_cache()
+
+# Create custom dataset file for final training
+result = {
+    "train": f"/gwhd_dataset/rs_train/images",
+    "val": f"/gwhd_dataset/val/images",
+    "test": f"/gwhd_dataset/test/images",
+    "nc": 1,
+    "names": ["wheat"],
+}
+
+with open("custom_dataset2.yaml", "w") as f:
+    dump = yaml.dump(result, default_flow_style=False)
+    f.write(dump)
+
+# Create the directory
+results_dir_path = 'results_5x_bt'
+
+try:
+	os.makedirs(results_dir_path, exist_ok=True) 
+	print(f"Directory '{results_dir_path}' created successfully.")
+except Exception as e:
+	print(f"An error occurred: {e}")
+
+# Specify paths to  configuration files
+cfg_yaml_path = 'yolo_5x_bt.yaml'
+data_yaml_path = 'custom_dataset2.yaml'
+hyp_yaml_path = 'hyp_5x_bt.yaml'
+weights = os.path.join('results_5x_bt/runs/train', 'exp_5x_bt1', 'weights', 'best.pt')
+project_path = os.path.join('results_5x_bt', 'runs', 'train')
+proj_name = 'exp_5x_bt2' 
+
+# Define the training parameters
+img_size = 800
+batch_size = 8
+epochs = 50
+es_epoch = 10
+
+print("Train Model:")
+
+# Command to execute the training script
+command = [
+    'python', 'YOLOv5/train.py',
+    '--img', str(img_size),
+    '--batch', str(batch_size),
+    '--epochs', str(epochs),
+    '--cfg', cfg_yaml_path,
+    '--data', data_yaml_path,
+    '--hyp', hyp_yaml_path,
+    '--weights', weights,
+    '--project', project_path,
+    '--name', proj_name,
+    '--patience', str(es_epoch),
+]
+
+# Open a file to write the output
+with open('train_5x_bt_log2.txt', 'w') as log_file:
+    result = subprocess.run(command, stdout=log_file, stderr=subprocess.STDOUT)
+
+if result.returncode != 0:
+    print(f"Command failed with return code {result.returncode}. See 'train_5x_bt_log2.txt' for details.")
+else:
+    print("Command executed successfully. See 'train_5x_bt_log2.txt' for details.")
+
+print("Training the Model Completed.")
+
+
+
---
+++ YOLOv5_BiFPN_TRANSFORMER/val_5x_bt_train.py
@@ -0,0 +1,50 @@
+import os
+import subprocess
+import torch 
+import yaml
+
+# Specify weights from initial training and path to custom dataset file
+weights=os.path.join('results_5x_bt/runs/train', 'exp_5x_bt1', 'weights', 'best.pt')
+data_yaml_path = 'custom_dataset1.yaml'
+
+# Specify validation output directory
+val_output_dir = 'results_5x_bt/runs/val'
+val_exp_name = 'exp_5x_bt1_train'
+
+# Define the validation parameters
+img_size = 800
+batch_size = 8
+conf_thres = 0.25
+iou_thres = 0.45
+task = 'train'
+
+print("Validate the Model:")
+
+# Command to execute the validation script
+val_command = [
+	'python', 'YOLOv5/val.py',
+	'--weights', weights,
+	'--data', data_yaml_path,
+	'--img', str(img_size),
+	'--batch', str(batch_size),
+	'--conf-thres', str(conf_thres),
+	'--iou-thres', str(iou_thres),
+	'--save-txt',
+	'--project', val_output_dir,
+	'--name', val_exp_name,
+	'--task', task1,
+	'--half'
+]
+
+# Open a file to write the output
+with open('val_5x_bt_log1_train.txt', 'w') as val_log_file:
+    result = subprocess.run(val_command, stdout=val_log_file, stderr=subprocess.STDOUT)
+
+if result.returncode != 0:
+    print(f"Validation command failed with return code {result.returncode}. See 'val_5x_bt_log1_train.txt' for details.")
+else:
+    print("Validation command executed successfully. See 'val_5x_bt_log1_train.txt' for details.")
+
+print("Validating the Model Completed.")
+print("See final metrics in val_5x_bt_log1_train.txt file")
+
